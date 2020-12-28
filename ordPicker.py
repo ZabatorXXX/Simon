cf1491cb9d82f0b8e38282411f4ea477b9dbba19
ZabatorXXX <42642927+ZabatorXXX@users.noreply.github.com>
2020-12-28 19:46:56 +0100

    Add files via upload

---
+++ ordPicker.py
@@ -0,0 +1,61 @@
+#def W_picker():
+import random
+
+A=['accruing','accurately','airman','artist']
+B=['buns', 'burdock', 'bureaux']
+C = ['competent', 'complain', 'compile']
+D = ['diner', 'dinosaur', 'dirt']
+E = ['earth', 'empty', 'edition']
+F = ['footstep', 'fibber', 'flop']
+G = ['gamma', 'gladly', 'glamour', 'greens']
+H = ['heirless', 'helix', 'helmet']
+I = ['inner', 'innocent', ' impulsive']
+J = ['jewel', 'junk', 'joey']
+K = ['kelp', 'ketchup', 'kryptonite']
+L = ['lion', 'locked', 'lynx']
+M = ['market', 'mini', 'motel']
+N = ['null', 'nun', 'nose']
+O = ['oak', 'overtook', 'owner']
+P = ['pool', 'prank', 'profit']
+Q = ['quote', 'quartz', 'quick']
+R = ['rock', 'ridiculous', 'rug']
+S = ['snooze', 'sort', 'species']
+T = ['tabletop', 'time', 'trollies']
+U = ['unbuilt', 'use', 'unstable']
+V = ['van', 'vent', 'volt']
+W = ['wardroom', 'wildcat', 'wtf']
+X = ['X-ray', 'xi', 'Xmas']
+Y = ['yard', 'yeti', 'yummy']
+Z = ['zen', 'zinc', 'zucchini']
+
+a4=random.choice(A)
+b3=random.choice(B)
+c3=random.choice(C)
+d3=random.choice(D)
+e3=random.choice(E)
+f3=random.choice(F)
+g4=random.choice(G)
+h3=random.choice(H)
+i3=random.choice(I)
+j3=random.choice(J)
+k3=random.choice(K)
+l3=random.choice(L)
+m3=random.choice(M)
+n3=random.choice(N)
+o3=random.choice(O)
+p3=random.choice(P)
+q3=random.choice(Q)
+r3=random.choice(R)
+s3=random.choice(S)
+t3=random.choice(T)
+u3=random.choice(U)
+v3=random.choice(V)
+w3=random.choice(W)
+x3=random.choice(X)
+y3=random.choice(Y)
+z3=random.choice(Z)
+
+print(a4, b3, c3, d3, e3, f3, g4, h3, i3, j3, k3, l3, m3, n3, o3, p3, q3, r3, s3, t3, u3, v3, w3, x3, y3, z3)
+
+#W_picker():
+
