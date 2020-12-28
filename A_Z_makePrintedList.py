3d36aa2acb05db25906bd86a427bf16eec062303
ZabatorXXX <42642927+ZabatorXXX@users.noreply.github.com>
2020-12-28 19:48:09 +0100

    Add files via upload

---
+++ A_Z_makePrintedList.py
@@ -0,0 +1,32 @@
+A = ['accruing', 'accurately', 'airman', 'artist']
+B = ['buns', 'burdock', 'bureaux'] 
+C = ['competent', 'complain', 'compile']
+D = ['diner', 'dinosaur', 'dirt']
+E = ['earth', 'empty', 'edition']
+F = ['footstep', 'fibber', 'flop']
+G = ['gamma', 'gladly', 'glamour', 'greens']
+H = ['heirless', 'helix', 'helmet']
+I = ['inner', 'innocent', ' impulsive']
+J = ['jewel', 'junk', 'joey']
+K = ['kelp', 'ketchup', 'kryptonite']
+L = ['lion', 'locked', 'lynx']
+M = ['market', 'mini', 'motel']
+N = ['null', 'nun', 'nose']
+O = ['oak', 'overtook', 'owner']
+P = ['pool', 'prank', 'profit']
+Q = ['quote', 'quartz', 'quick']
+R = ['rock', 'ridiculous', 'rug']
+S = ['snooze', 'sort', 'species']
+T = ['tabletop', 'time', 'trollies']
+U = ['unbuilt', 'use', 'unstable']
+V = ['van', 'vent', 'volt']
+W = ['wardroom', 'wildcat', 'wtf']
+X = ['X-ray', 'xi', 'Xmas']
+Y = ['yard', 'yeti', 'yummy']
+Z = ['zen', 'zinc', 'zucchini']
+
+0 - 25
+
+
+A.extend(B + C + D + F + G + H + I + J + K + L + M + N + O + P + Q + R + S + T + U + V + W + X + Y + Z) 
+print(A)
