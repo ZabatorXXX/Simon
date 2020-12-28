4ae10447ea8d4ab2f021544ea6d0fe7e43ca12ac
ZabatorXXX <42642927+ZabatorXXX@users.noreply.github.com>
2020-12-28 19:48:47 +0100

    Add files via upload

---
+++ MoreComplex.py
@@ -0,0 +1,42 @@
+import random
+''' 
+A=['accruing','accurately','airman','artist']
+B=['buns', 'burdock', 'bureaux']
+C = ['competent', 'complain', 'compile']
+D = ['diner', 'dinosaur', 'dirt']
+E = ['earth', 'empty', 'edition']
+F = ['footstep', 'fibber', 'flop']
+G = ['gamma', 'gladly', 'glamour', 'greens']
+H = ['heirless', 'helix', 'helmet']
+I = ['inner', 'innocent', ' impulsive']
+J = ['jewel', 'junk', 'joey']
+K = ['kelp', 'ketchup', 'kryptonite']
+L = ['lion', 'locked', 'lynx']
+M = ['market', 'mini', 'motel']
+N = ['null', 'nun', 'nose']
+O = ['oak', 'overtook', 'owner']
+P = ['pool', 'prank', 'profit']
+Q = ['quote', 'quartz', 'quick']
+R = ['rock', 'ridiculous', 'rug']
+S = ['snooze', 'sort', 'species']
+T = ['tabletop', 'time', 'trollies']
+U = ['unbuilt', 'use', 'unstable']
+V = ['van', 'vent', 'volt']
+W = ['wardroom', 'wildcat', 'wtf']
+X = ['X-ray', 'xi', 'Xmas']
+Y = ['yard', 'yeti', 'yummy']
+Z = ['zen', 'zinc', 'zucchini']
+'''
+
+class W_from_englishDictionary:
+    def getChoise(self):
+        return random.choice(open("/usr/share/dict/words","r").readlines())
+       # return random.choice(A + B + C + D + E + F + G + H + I + J + K + L + M + N + O + P + Q + R + S + T + U + V + W + X + Y + Z)
+
+    def W_picker(self):
+        for n in range(40):
+            print(self.getChoise())
+                
+            
+picker = W_from_englishDictionary()
+picker.W_picker()
